20b730b3dfc154e0e87f219de03de68226af5d1d
Fueda <fueda.g.aa@m.titech.ac.jp>
2019-01-06 21:12:36 +0900

    カレンダー(CalendarView)とボタンを配置

--- app/src/main/java/jp/ac/titech/itpro/sed/didyoufood/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sed/didyoufood/MainActivity.java
@@ -1,13 +1,39 @@
 package jp.ac.titech.itpro.sed.didyoufood;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    CalendarView mCalendarView;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
+        String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        display(date);
+        */
+
+        CalendarView mCalendarView = (CalendarView)findViewById(R.id.calendar);
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = dayOfMonth + "-" + "0" + (month + 1) + "-" + year;
+                display(date);
+            }
+        });
     }
+    public void display(String text){
+        TextView textView = (TextView)findViewById(R.id.text);
+        textView.setText("" + text);
+    }
+
 }
